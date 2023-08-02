6df3a3ee7aa07a4af11ee71fe8301c55facc2a0a
piekarskipiotr <ppiekarski8@gmail.com>
2023-08-02 20:47:45 +0200

    Remove current user from a friend's search list

--- lib/pages/friends/cubit/friends_cubit.dart
+++ lib/pages/friends/cubit/friends_cubit.dart
@@ -2,14 +2,17 @@ import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
+import 'package:pooapp/data/repositories/auth_repository.dart';
 import 'package:pooapp/data/repositories/firestore_repository.dart';
 import 'package:pooapp/pages/friends/cubit/friends_state.dart';
 
-@lazySingleton
+@injectable
 class FriendsCubit extends Cubit<FriendsState> {
-  FriendsCubit(this._firestoreRepository) : super(ReturningSearchData(null));
+  FriendsCubit(this._firestoreRepository, this._authRepository)
+      : super(ReturningSearchData(null));
 
   final FirestoreRepository _firestoreRepository;
+  final AuthRepository _authRepository;
   Timer? _debounce;
 
   Future<dynamic> search(String? username) async {
@@ -18,9 +21,12 @@ class FriendsCubit extends Cubit<FriendsState> {
       try {
         emit(Searching());
         if (username != null && username.isNotEmpty) {
-          await _firestoreRepository.searchForUsers(username).then(
-                (users) => emit(ReturningSearchData(users)),
-              );
+          final currentUser = _authRepository.getCurrentUser();
+          if (currentUser == null) throw Exception('user-not-sign-in');
+
+          final users = await _firestoreRepository.searchForUsers(username);
+          users?.removeWhere((user) => user.id == currentUser.uid);
+          emit(ReturningSearchData(users));
         }
       } catch (e) {
         emit(SearchingFailed(e.toString()));
